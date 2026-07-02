5e2ea32c21915c83785c2a3ebbbeef340a9f9c32
Haronq <hendrikhsu2020@gmail.com>
2026-07-01 20:44:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test text</title>
+  <link rel="stylesheet" href="style.css">
 document.getElementById("maker")
 var user="white"
 function newer(){
@@ -5,3 +12,9 @@ user = prompt("What color would you like the background to be?")
   document.body.style.backgroundColor=user
 }
 document.getElementById("maker").addEventListener("click",newer)
+
+</head>
+<body>
+<button id="maker">New Color</button>
+</body>
+</html>
